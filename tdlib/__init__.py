5685312a5c6eb86c7ff76624fee760c6a4a2fd28
coda <822628+mayli@users.noreply.github.com>
2024-08-29 10:28:47 +0000

    m

--- tdlib/__init__.py
+++ tdlib/__init__.py
@@ -1,11 +1,21 @@
+import os
+import sys
+
 try:
     from importlib import resources
+
     resources.files
 except (ImportError, AttributeError):
     import importlib_resources as resources
 
-def _get_tdjson_lib_path() -> str:
 
-    lib_name = "libtdjson.so"
+def _get_tdjson_lib_path() -> str:
+    if sys.platform.startswith("linux"):  # could be "linux", "linux2", "linux3", ...
+        lib_name = "lib/libtdjson.so"
+    elif sys.platform == "darwin":
+        # MAC OS X
+        lib_name = "lib/libtdjson.dylib"
+    elif os.name == "nt":
+        lib_name = "lib/libtdjson.dll"
 
     return str(resources.files("tdlib").joinpath(lib_name))
