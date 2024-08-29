930811f9d45a30702c44e827747b01aa033649dc
coda <822628+mayli@users.noreply.github.com>
2024-08-29 09:11:26 +0000

    use pyproject

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-import shutil
 import os
+import shutil
 from pathlib import Path
-from setuptools import setup, Extension
-from wheel.bdist_wheel import bdist_wheel
 from subprocess import check_call
 
+from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext as build_ext_orig
+from wheel.bdist_wheel import bdist_wheel
 
-EXTRA = '-DCMAKE_TOOLCHAIN_FILE=%s/vcpkg/scripts/buildsystems/vcpkg.cmake' % os.getcwd()
+EXTRA = "-DCMAKE_TOOLCHAIN_FILE=%s/vcpkg/scripts/buildsystems/vcpkg.cmake" % os.getcwd()
+
 
 def get_version():
     # project(TDLib VERSION 1.8.35 LANGUAGES CXX C)
-    with open('tdlight/CMakeLists.txt') as fd:
+    with open("tdlight/CMakeLists.txt") as fd:
         for line in fd:
-            if line.startswith('project(TDLib VERSION'):
+            if line.startswith("project(TDLib VERSION"):
                 return line.split()[2]
+    return "0.0.1"
+
 
 class bdist_wheel_abi3(bdist_wheel):
     def get_tag(self):
@@ -26,41 +29,36 @@ class bdist_wheel_abi3(bdist_wheel):
 
         return python, abi, plat
 
+
 class build_ext(build_ext_orig):
     def run(self):
-        prefix = Path("build")
-        prefix.mkdir(exist_ok=True)
-        target = Path(self.build_lib) / "tdlib" / "libtdjson.so"
+        pass
 
-        extra = EXTRA if os.name == 'nt' else ''
-
-        check_call(f"cmake -DCMAKE_BUILD_TYPE=Release ../tdlight {extra}", cwd=prefix, shell=True)
-        check_call("cmake --build . -j $(nproc) --target tdjson", cwd=prefix, shell=True)
-
-        src = prefix / 'libtdjson.so'
-        shutil.copy(src, target, follow_symlinks=True)
 
 def read_readme():
-    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
+    ) as f:
         return f.read()
+
 
 setup(
     name="tdlib-binary",
     version=get_version(),
     description="tdlib binary build",
     long_description=read_readme(),
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="tdlib-binary dev",
     packages=["tdlib"],
     ext_modules=[Extension("tdlib", [])],
     cmdclass={
         "build_ext": build_ext,
         "bdist_wheel": bdist_wheel_abi3,
-        },
+    },
     package_data={
-        "tdlib": ["tdjson.*"],
+        "tdlib": ["tdjson.so", "tdjson.dll", "libtdjson.dylib"],
     },
     install_requires=[
         'importlib-resources>=1.3; python_version < "3.9"',
-    ]
+    ],
 )
